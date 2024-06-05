12780c1bd47835f93abcac47dc5460011881f195
KseniyaBoychenko2023 <kcyxa.boychenko@gmail.com>
2024-06-05 18:13:47 +0800

    API

--- files/js/hah.js
+++ files/js/hah.js
@@ -3,6 +3,8 @@
     function init() {
         document.getElementById("light").addEventListener("click", lightTheme);
         document.getElementById("dark").addEventListener("click", darkTheme);
+        document.getElementById("show").addEventListener("click", showGP);
+        document.getElementById("next").addEventListener("click", nextGP);
     }
 
     const html = document.querySelector("html");
@@ -28,4 +30,46 @@
             html.setAttribute("data-theme", "dark");
         }
     }
+
+    let url = "https://hp-api.herokuapp.com/api/characters";
+    function showGP() {
+        fetch(url)
+            .then(checkStatus)
+            .then(resp => resp.json())
+            .then(showResult)
+            .catch(console.error);
+    }
+
+    let n = 0;
+    function showResult(response) {
+        let character = response[n];
+        let nameElement = document.getElementById("name");
+        let div = document.getElementById("img");
+        nameElement.textContent = '';
+        div.innerHTML = '';
+        let img = document.createElement('img');
+        img.className = "i";
+        img.src = character.image;
+        div.appendChild(img);
+        nameElement.textContent = character.name;
+    }
+
+    function nextGP() {
+        if(n <= 20) {
+            n++;
+        }
+        else {
+            n = 0;
+        }
+        showGP();
+    }
+
+    function checkStatus(response) {
+        if (!response.ok) {
+            console.log("error");
+            throw Error("Ошибка запроса: " + response.statusText);
+        }
+        return response;
+    }
+
 })();
